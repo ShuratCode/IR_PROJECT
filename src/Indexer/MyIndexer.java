640dfa5be31578ab48383962dda4f5502e549a45
Elior <Nehemya@post.bgu.ac.il>
2017-12-17 10:17:47 +0200

    minor changes in view-> save D&C

--- src/Indexer/MyIndexer.java
+++ src/Indexer/MyIndexer.java
@@ -172,7 +172,7 @@ public class MyIndexer {
             }
             fnCloseBufferReader(bfrForFiles);
 
-            fnDeleteTempFiles(files);
+            //fnDeleteTempFiles(files);
             raf.close();
 
             fnWriteTemp();
